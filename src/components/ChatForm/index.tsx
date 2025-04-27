54cdbbb3a2298d28a87437c75f67e090bdabd76c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-04-27 15:33:46 -0300

    ✨ feat: adiciona tipagem e suporte a controle de input no formulário

--- src/components/ChatForm/index.tsx
+++ src/components/ChatForm/index.tsx
@@ -1,12 +1,22 @@
+'use client'
+
 import { IconSend } from '@/components/Icons'
 import styles from './chat.module.css'
 
-export const ChatForm = () => {
+interface ChatFormProps {
+    input: string
+    onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+    onSubmit: (e: React.FormEvent<HTMLFormElement>) => void
+}
+
+export const ChatForm = ({ input, onInputChange, onSubmit }: ChatFormProps) => {
     return (
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={onSubmit}>
             <input
                 className={styles.input}
                 placeholder="Digite sua mensagem..."
+                value={input}
+                onChange={onInputChange}
                 required
             />
             <button className={styles.btn}>
